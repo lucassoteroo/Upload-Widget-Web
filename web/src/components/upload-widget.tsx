a41c6dd0b51b0f1270e682663b2574660051879e
Lucas Sotero <lucas.abreu2@fit-tecnologia.org.br>
2026-01-27 20:14:46 -0400

    setup inicial do projeto

---
+++ web/src/components/upload-widget.tsx
@@ -0,0 +1,29 @@
+import * as Collapsible  from "@radix-ui/react-collapsible";
+import { UploadWidgetDropzone } from "./upload-widget-dropzone";
+import { UploadWidgetHeader } from "./upload-widget-header";
+import { UploadWidgetUploadList } from "./upload-widget-upload-list";
+import { useState } from "react";
+import { UploadWidgetMinimizedButton } from "./upload-widget-minimized-button";
+
+export function UploadWidget() {
+    const [ isWidgetOpen, setIsWidgetOpen ] = useState(false)
+
+    return (
+        <Collapsible.Root onOpenChange={setIsWidgetOpen}>
+            <div className="bg-zinc-900 overflow-hidden w-90 rounded-xl shadow-shape">
+                {!isWidgetOpen && <UploadWidgetMinimizedButton />}
+
+                <Collapsible.Content>
+                    <UploadWidgetHeader></UploadWidgetHeader>
+
+                    <div className="flex flex-col gap-4 py-3">
+                        <UploadWidgetDropzone />
+                        <div className="h-px bg-zinc-800 border-t border-black/50 box-content"></div>
+                        <UploadWidgetUploadList />
+                    </div>
+                </Collapsible.Content>
+
+            </div>  
+        </Collapsible.Root>
+    )
+}
