3941cc569619ba9d6870a44466650504e2f277fe
Lucas Sotero <lucas.abreu2@fit-tecnologia.org.br>
2026-01-27 20:49:45 -0400

    add animações de componentes

--- web/src/components/upload-widget.tsx
+++ web/src/components/upload-widget.tsx
@@ -2,15 +2,34 @@ import * as Collapsible  from "@radix-ui/react-collapsible";
 import { UploadWidgetDropzone } from "./upload-widget-dropzone";
 import { UploadWidgetHeader } from "./upload-widget-header";
 import { UploadWidgetUploadList } from "./upload-widget-upload-list";
-import { useState } from "react";
 import { UploadWidgetMinimizedButton } from "./upload-widget-minimized-button";
+import { motion, useCycle } from "motion/react";
 
 export function UploadWidget() {
-    const [ isWidgetOpen, setIsWidgetOpen ] = useState(false)
+    const [ isWidgetOpen, toggleWidgetOpen ] = useCycle(false, true)
 
     return (
-        <Collapsible.Root onOpenChange={setIsWidgetOpen}>
-            <div className="bg-zinc-900 overflow-hidden w-90 rounded-xl shadow-shape">
+        <Collapsible.Root onOpenChange={() => toggleWidgetOpen()}>
+            <motion.div 
+                className="bg-zinc-900 overflow-hidden max-w-90 rounded-xl shadow-shape"
+                animate={isWidgetOpen ? 'open' : 'closed'}
+                variants={{
+                    closed: {
+                        width: 'max-content',
+                        height: 44,
+                        transition: {
+                            type: 'inertia'
+                        }
+                    },
+                    open: {
+                        width: 360,
+                        height: 'auto',
+                        transition: {
+                            duration: 0.2
+                        }
+                    }   
+                }}
+                >
                 {!isWidgetOpen && <UploadWidgetMinimizedButton />}
 
                 <Collapsible.Content>
@@ -22,8 +41,7 @@ export function UploadWidget() {
                         <UploadWidgetUploadList />
                     </div>
                 </Collapsible.Content>
-
-            </div>  
+            </motion.div>  
         </Collapsible.Root>
     )
 }
