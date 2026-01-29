66a1cbd5fb6ce4777f4edbd2a3689b20580a5896
Lucas Sotero <lucas.abreu2@fit-tecnologia.org.br>
2026-01-29 08:33:40 -0400

    add funcionalidades de cancelamento e retentativa de envio de arquivos

--- web/src/http/upload-file-to-storage.ts
+++ web/src/http/upload-file-to-storage.ts
@@ -4,8 +4,15 @@ interface UploadFileToStorageParams {
     file: File
 }
 
+interface UploadFileToStorageOpts {
+    signal?: AbortSignal
+}
+
 // função que faz o conexão com a rota post uploads
-export async function uploadFileToStorage({ file } : UploadFileToStorageParams) {
+export async function uploadFileToStorage(
+    { file } : UploadFileToStorageParams,
+    opts?: UploadFileToStorageOpts
+) {
     const data = new FormData()
 
     data.append('file', file)
@@ -13,7 +20,8 @@ export async function uploadFileToStorage({ file } : UploadFileToStorageParams) 
     const response = await axios.post<{ url: string }>('http://localhost:3333/uploads', data, {
         headers: {
             'Content-Type': 'multipart/form-data'
-        }
+        },
+        signal: opts?.signal
     })
 
     return { url: response.data.url }
--- web/src/store/uploads.ts
+++ web/src/store/uploads.ts
@@ -6,12 +6,15 @@ import { uploadFileToStorage } from '../http/upload-file-to-storage';
 
 export type Upload = {
     name: string,
-    file: File
+    file: File,
+    abortController: AbortController
+    status: 'progress' | 'success' | 'error' | 'canceled'
 }
 
 type UploadState = {
     uploads: Map<string, Upload>
     addUploads: (file: File[]) => void
+    cancelUpload: (uploadId: string) => void
 }
 
 enableMapSet()
@@ -26,30 +29,71 @@ export const useUploads = create<UploadState, [['zustand/immer', never]]>(
                 return
             }
 
-            await uploadFileToStorage({ file: upload.file })
+            try {
+                await uploadFileToStorage(
+                    { file: upload.file },
+                    { signal: upload.abortController.signal }
+                )
+    
+                set(state => {
+                    state.uploads.set(uploadId, {
+                        ...upload,
+                        status: 'success'
+                    })
+                })            
+            } catch {
+                set(state => {
+                    state.uploads.set(uploadId, {
+                        ...upload,
+                        status: 'error'
+                    })
+                })      
+            }
         }
-
+        
         function addUploads(files: File[]) {
             for (const file of files) {
                 const uploadId = crypto.randomUUID()
-
+                const abortController = new AbortController()
+                
                 const upload: Upload = {
                     name: file.name,
-                    file
+                    file,
+                    abortController,
+                    status: 'progress'
                 }
-
+                
                 // atribui um valor ao estado
                 set(state => {
                     state.uploads.set(uploadId, upload)
                 })
-
+                
                 processUpload(uploadId)
             }
+        }
+        
+        function cancelUpload(uploadId: string) {
+            const upload = get().uploads.get(uploadId)
+
+            if (!upload) {
+                return
+            }
+
+            upload.abortController.abort()
+
+            // atribui um valor ao estado
+            set(state => {
+                state.uploads.set(uploadId, {
+                    ...upload,
+                    status: 'canceled'
+                })
+            })
         }
 
         return {
             uploads: new Map(),
             addUploads,
+            cancelUpload
         }
     })
 )
